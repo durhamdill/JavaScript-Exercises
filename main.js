5e2d70f9b6c6c607f0596bed932ab4e483edc84c
durhamdill <durhamdill@Kathryns-MacBook-Pro.local>
2017-01-06 19:54:39 -0500

    exercise3
    working with numbers; not sure i did what was asked on the last thing

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*=========================================================
     Part 1: Variables
+    Exercise 1
 */
 var givenName;
 console.log(givenName);
@@ -24,3 +25,36 @@ console.log(givenName);
 var givenName = "Brett";
 var greeting = ("Hello, how are you " + givenName + "?");
 console.log(greeting);
+
+/*=========================================================
+     Part 2: Simple Math
+     Exercise 3
+*/
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+console.log(math);
+// Q: What is `math` set to?
+// A: 40
+
+math = high - "5";
+console.log(math);
+// Q: What is `math` set to?
+// A: 45
+
+
+// Using the `high` & `low` Variables, work through each of the 4 math operators below and log the answers to the console.
+// Write Code Below:
+math = low - high;
+console.log(math);
+
+math = high + low;
+console.log(math);
+
+math = high / low;
+console.log(math);
+
+math = high * low;
+console.log(math);
