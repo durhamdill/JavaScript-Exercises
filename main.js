cd3cacf4a9f105e8c20acb7ab14b1959dfcda57d
durhamdill <durhamdill@Kathryns-MacBook-Pro.local>
2017-01-06 19:38:27 -0500

    exercise1
    completed answers for exercise one; used console.log to confirm

---
+++ main.js
@@ -0,0 +1,19 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+console.log(givenName);
+// Q: What value is stored in `givenName` right now?
+// A: none, it is currently undefined
+
+
+givenName = "Brett";
+console.log(givenName);
+// Q: What is `givenName` set to now?
+// A: Brett
+
+
+givenName = givenName;
+console.log(givenName);
+// Q: What is `givenName` set to now?
+// A: Brett
